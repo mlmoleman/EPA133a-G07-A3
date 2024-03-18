a7fb6a8261052d994683947c0734bec4c2efaf40
mlmoleman <M.L.Moleman@student.tudelft.nl>
2024-03-18 21:48:21 +0100

    Network update

--- model/intersections.py
+++ model/intersections.py
@@ -188,3 +188,87 @@ df_intersections_side["type"] = "intersection"
 
 output_csv_path = os.path.join(project_root, 'data', 'intersections_side.csv')
 df_intersections_side.to_csv(output_csv_path, index=False)
+
+###
+###
+###
+
+###
+### Modify bridge data
+###
+
+# import bridge data
+df_bridges = pd.read_csv('../data/bridges_cleaned.csv')
+# drop old column named index
+df_bridges = df_bridges.drop(["index", "Unnamed: 0"], axis='columns')
+# add intersection to column
+df_bridges['intersec_to'] = None
+# reposition columns
+df_bridges = df_bridges[['road', 'km', 'type', 'model_type', 'name', 'length', 'condition', 'lat', 'lon', 'intersec_to']]
+
+###
+### Modify intersection data, align with bridge data
+###
+
+# import intersections data
+df_intersections = pd.read_csv('../data/intersections_main.csv')
+# change chainage into km
+df_intersections.rename({'chainage': 'km'}, axis=1, inplace=True)
+# create model_type
+df_intersections['model_type'] = 'intersection'
+# create length
+df_intersections['length'] = 0
+# align intersection columns with bridge colum
+# create condition
+df_intersections['condition'] = None
+# format colum,ns
+df_intersections = df_intersections[['road', 'km', 'type', 'model_type', 'name', 'length', 'condition', 'lat', 'lon', 'intersec_to']]
+
+###
+### Check if intersected road is longer than 25 km
+###
+
+# get all intersected roads
+intersections = df_intersections['intersec_to'].unique().tolist()
+# remove nan if present
+intersections = [x for x in intersections if str(x) != 'nan']
+# for each road which is an intersection of N1 or N2
+for intersection in intersections: 
+    # subset all data points for the road
+    road_subset = df_bridges[df_bridges['road'] == intersection]
+    # get last chainage
+    road_length = road_subset.iloc[-1]['km']
+    if road_length < 25: 
+        intersections.remove(intersection)
+# remove left-overs by hand
+intersections.remove('N120')
+intersections.remove('N209')
+
+###
+### Merge bridges with intersection points
+###
+
+# keep all bridges in roads that intersect
+df_bridges = df_bridges[df_bridges['road'].isin(intersections)]
+# keep all roads which are still in intersections list, so longer than 25 km
+df_intersections = df_intersections[df_intersections['intersec_to'].isin(intersections)] 
+# get the dataframes which needs to be merged
+frames = [df_bridges, df_intersections]
+# merge dataframes into df
+df = pd.concat(frames)
+
+###
+### Format data
+###
+
+# sort roads dataframe based on road name and chainage
+df = df.sort_values(by=['road', 'km'])
+# reset index
+df = df.reset_index(drop=True) 
+# get index to remove, which is a duplicate for N2 
+index_to_remove = df[(df['road'] == 'N2') & (df['km'] == 0.0) & (df['model_type'] == 'sourcesink')].index
+# drop index
+df = df.drop(df[(df['road'] == 'N2') & (df['km'] == 0.0) & (df['model_type'] == 'sourcesink')].index)
+
+# save merged dataframe as CSV file
+df.to_csv('../data/bridges_cleaned_intersected_long.csv')
