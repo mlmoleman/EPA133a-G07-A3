22f700f3c4ad6c0ae5b649701d5586bc5c4ec261
roesel1 <roelofkooijman@gmail.com>
2024-03-21 23:46:53 +0100

    Intersectionale wegoversteking

---
+++ model/Bonus_assigment.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import contextily as cx
+from shapely import distance
+from pyproj import Transformer
+from shapely.geometry import LineString, MultiPoint
+import shapely.ops as sp_ops
+import warnings
+import os
+from shapely import buffer
+
+os.environ['USE_PYGEOS'] = '0'
+warnings.filterwarnings("ignore")
+
+# Gets the path to the main folder
+main_folder_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+# Reads the road data
+df = gpd.read_file(main_folder_path + "\data\gis\osm\\roads.shp")
+# Selects all the unique values in the ref column
+road_names = df[df["ref"].notnull()]["ref"].unique()
+# Select the rows that start with an N, indicating the N roads
+road_names_N = [road for road in road_names if road.startswith("N")]
+
+# Subsets a dataframe wit hall the N road data
+df_N = df[df["ref"].isin(road_names_N)]
+# Removes the spaces between the N and the number in some rows
+df_N["ref"] = df_N["ref"].str.replace(" ", "")
+
+# Creates a dataframe that contains the road sections for the N1 and N2
+df_primary = df[(df["type"] == "primary") | (df["type"] == "trunk")]
+# Gets the intersections that are calculated for the main model
+df_sections = pd.read_csv(main_folder_path + "\\data\\intersections_BONUS.csv")
+# Convert the geometry back into working shapely geometry
+df_sections["geometry"] = df_sections["0"].apply(wkt.loads)
+# Turns the df into a gdf
+df_sections = gpd.GeoDataFrame(df_sections, geometry="geometry", crs="EPSG:4326").drop(columns="0")
+
+# Will contain buffered versions of the intersections
+points = []
+# Loops over all the intersection points
+for point in df_sections["geometry"]:
+    point = buffer(point, 0.1)
+    points.append(point)
+
+# Removes the spaces between the N and the number in some rows
+df_primary["ref"] = df_primary["ref"].str.replace(" ", "")
+
+# Subsets a dataframe for only the N1 and N2
+df_N1N2 = df_primary[(df_primary["ref"] == "N1") | (df_primary["ref"] == "N2")]
+
+# Loads in the road data and turns it into a geopandas dataframe
+df_normal_data = pd.read_csv(main_folder_path + "\\data\\_roads3.csv")
+gdf_normal = gpd.GeoDataFrame(df_normal_data, geometry=gpd.points_from_xy(df_normal_data.lon, df_normal_data.lat),
+                              crs="EPSG:4326")
+
+# Will contain the lines of the N1 and N2
+lines = []
+# Loops over the N1 and N2 name
+for name in ["N1", "N2"]:
+    # Subsets for the road
+    gdf_temp = gdf_normal[gdf_normal["road"] == name]
+    # Buffers the line
+    line = buffer(LineString(gdf_temp["geometry"]), 0.1)
+    # Puts into the list
+    lines.append(line)
+
+# Will contain the index of the lines that intersects with the buffered N1 or N2
+cross_index_N1 = []
+cross_index_N2 = []
+# Loops over all the primary nad trunk roads
+for index in df_primary.index:
+    # Check if the road intersects with the N1
+    if lines[0].intersects(df_primary.loc[index, "geometry"]):
+        # If yes add to the list
+        cross_index_N1.append(index)
+    # Check if the road intersects with the N2
+    if lines[1].intersects(df_primary.loc[index, "geometry"]):
+        # If yes add to the list
+        cross_index_N2.append(index)
+
+# Select the lines that intersect with the N1 and N2
+intersecting_N1 = df_primary[df_primary.index.isin(cross_index_N1)]
+intersecting_N2 = df_primary[df_primary.index.isin(cross_index_N2)]
+# Merge the lines for the N1 and N2 into one dataframe
+intersecting_N1N2 = pd.concat([intersecting_N1, intersecting_N2])
+# Removes the Spaces and all the segments of the N1 and N2 itself
+intersecting_N1N2 = intersecting_N1N2[~intersecting_N1N2["ref"].str.replace(" ", "").isin(["N1", "N2"])]
+
+# Will contain the intersection points calculated with the shapefile
+Intersect_points = []
+# Loops over the lines segements of the N1 and N2
+for line_N1N2 in df_N1N2["geometry"]:
+    # loops over the lines that intersect with the N1 and N2
+    for intersect_line in intersecting_N1N2["geometry"]:
+        # If the segments are not the same
+        if line_N1N2 != intersect_line:
+            # If the lines intersect
+            if line_N1N2.intersects(intersect_line):
+                # Calcualte the intersection point
+                Intersect_points.append(line_N1N2.intersection(intersect_line))
+
+# Makes and GeoSeries from the points that intersect with the N1 and N2
+shape_file_intersects = gpd.GeoSeries(Intersect_points, crs="4326")
+
+# Will contain the pairs of the intersection points based on the csv data and shapefile data
+final_pair = []
+
+# Loops over the intersections of the csv data
+for section in df_sections["geometry"]:
+    # Indicates the smallest distance
+    min_dist = 10000
+    # Loops over the all the intersection points of the shapefile data
+    for shape_file_intersect in shape_file_intersects.unique():
+        # Checks if there are multiple intersections
+        if isinstance(shape_file_intersect, MultiPoint):
+            # If yes, it will loop over the multiple intersections
+            for point in list(shape_file_intersect.geoms):
+                # Calculate distance
+                dist = distance(section, point)
+                # Check if it is smaller then the already smallest distance
+                if dist < min_dist:
+                    # If yes, update the current smallest distance
+                    min_dist = dist
+                    # Change the current pair that have the smallest distance
+                    closest_point_pair = (section, point)
+        # If there are no multiple intersections
+        else:
+            # Calculate distance
+            dist = distance(section, shape_file_intersect)
+            # If yes, update the current smallest distance
+            if dist < min_dist:
+                # If yes, update the current smallest distance
+                min_dist = dist
+                # Change the current pair that have the smallest distance
+                closest_point_pair = (section, shape_file_intersect)
+    # After looping over every combination, the pair with the smallest distance is added to the list
+    final_pair.append(closest_point_pair)
+
+# Will contain the lengths that is between the intersection points from the csv data and the intersection points from the shapefile
+dict_length = {}
+# Transformer that can convert to another crs
+transformer = Transformer.from_crs('EPSG:4326', 'EPSG:3857', always_xy=True)
+# Loops over the pairs
+for index, pair in enumerate(final_pair):
+    #Turns  the pair into a line
+    line = LineString([pair[0], pair[1]])
+    # Converts the line to crs that uses meters and then calculates the distance
+    line_length = sp_ops.transform(transformer.transform, line).length
+    # Add the distance with its road name to the dict
+    dict_length[df_sections["Unnamed: 0"][index]] = line_length / 1000
--- model/intersections.py
+++ model/intersections.py
@@ -1,158 +1,140 @@
-# Import packages
+import pandas as pd
+from shapely import distance
+from pyproj import Transformer
+from shapely.geometry import LineString
+import shapely.ops as sp_ops
+import warnings
+from shapely import buffer
 import os
 
 os.environ['USE_PYGEOS'] = '0'
-import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import contextily as cx
-from shapely.geometry import Point, LineString, shape
-import numpy as np
-from shapely import distance
 
-###
-### Importing road data
-###
+warnings.filterwarnings("ignore")
 
-# Selects the correct file path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.dirname(current_dir)
-path_road = os.path.join(project_root, 'data', '_roads3.csv')
+# Gets the path to main folder
+main_folder_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+# Reads in the road data and turns it into a geodataframe
+df = pd.read_csv(main_folder_path + "\\data\\_roads3.csv")
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326")
 
-# opens csv file as df and the converts it to a geodataframe
-df = pd.read_csv(path_road)
-gdf = gpd.GeoDataFrame(
-    df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326")
-
-###
-###
-###
-
-###
-### Create LineStrings from all the N roads.
-###
-
-# Filters all the different road names and picks the ones starting with a N
+# Creates a list of all the unique road names
 road_names = gdf["road"].unique()
-road_names_N1 = [road for road in road_names if road.startswith("N")]
+# Select the rows that start with N to get all the N roads
+road_names_N = [road for road in road_names if road.startswith("N")]
 
-# Dictionary will contain the linestrings with the road name as key
+# Transformer that can be used to convert from a lat,lon based crs to a meters one
+transformer = Transformer.from_crs('EPSG:4326', 'EPSG:3857', always_xy=True)
+# Will contain the buffered lines made for every road
+line_dict_buffer = {}
+# Will contain the lines made for every road
 line_dict = {}
 # Loops over all the road names
-for road_name in road_names_N1:
-    # Selects the rows that correspond to the road name
+for road_name in road_names_N:
+    # Selects the rows corresponding to the road name
     bridges_road = gdf[gdf["road"] == road_name]
-    # Only considers roads with two or more points, can not make a line from 1 point
+    # Check if the road has more then two rows, can not make a line from one point
     if len(bridges_road["geometry"]) >= 2:
-        # Creates a line from the points and add them
-        line_dict[road_name] = LineString(bridges_road["geometry"])
-# Turns the dictionary into a geopandas series
-gseries = gpd.GeoSeries(line_dict.values(), index=line_dict.keys(), crs=4326)
+        # Creates line
+        line = LineString(bridges_road["geometry"])
+        # Converts to meter based crs and then get the length in meters
+        line_transformed = sp_ops.transform(transformer.transform, line).length
+        # If line is longer then 25km then add to dict
+        if line_transformed > 25000:
+            line_dict_buffer[road_name] = buffer(LineString(bridges_road["geometry"]), 0.01)
+            line_dict[road_name] = line
 
-###
-###
-###
+# Creates GeoPandasSeries of the line and buffered line dictionaries
+gseries = gpd.GeoSeries(line_dict_buffer.values(), index=line_dict_buffer.keys(), crs=4326)
+gseries_test = gpd.GeoSeries(line_dict.values(), index=line_dict.keys(), crs=4326)
 
-###
-### Checks which roads intersect with the N1 or N2
-###
-
-
-# Will contain the road names that intersect with the N1
+# Will contain the indexes of the roads that intersect with the N1
 cross_index_N1 = []
-# Loops over all the lines of the roads
+# Loops over the roads in the gseries
 for index in gseries.index:
-    # If the road intersects then it will add it to list
+    # Checks if the road intersects yes or no
     if gseries["N1"].intersects(gseries[index]):
+        # If yes then add the index to the cross_index_N1 list
         cross_index_N1.append(index)
 
-# Same as above but then for the N2
+# Same as above, but then for the N2
 cross_index_N2 = []
 for index in gseries.index:
     if gseries["N2"].intersects(gseries[index]):
         cross_index_N2.append(index)
 
-# Select the lines that intersect with the N1 and N2
+# Selects the lines that intersect with the N1 and N2
 intersecting_N1 = gseries[gseries.index.isin(cross_index_N1)]
 intersecting_N2 = gseries[gseries.index.isin(cross_index_N2)]
-
-# Calculates the coordinates where the roads intersect
+# Calculates the intersection points. N1 and N2 get dropped, because they intersect
+# with themselves and there is no need to examine an intersection with itself
 intersections_N1 = gseries["N1"].intersection(intersecting_N1).drop("N1")
 intersections_N2 = gseries["N2"].intersection(intersecting_N2).drop("N2")
 
-# Creates gdf's with the rows corresponding to the N1 and N2
+# Create subset of the rows for the N1 and N2
 gdf_N1 = gdf[gdf["road"] == "N1"]
 gdf_N2 = gdf[gdf["road"] == "N2"]
+# Saves the intersection points for the bonus assignment
+pd.concat([intersections_N1, intersections_N2]).to_csv(main_folder_path + "\\data\\intersections_BONUS.csv")
 
-###
-###
-###
 
-###
-### Gets the closest LRP on the N1 to the intersection of the two roads
-###
-
-# Will contain the rows of the points on the N1 closest the intersection
-df_list = []
-road_name_list = []
-# Will contain the indexes of the points on the N1 closest the intersection
-closest_points = []
-# Loop over the needed data for the N1 and then the N2
-for gdf_data, intersect_data in [[gdf_N1, intersections_N1], [gdf_N2, intersections_N2]]:
-    # Loops over the the intersection point indexes
+#
+def process_intersection_data(gdf_data, intersect_data):
+    """
+    Used to get the closest LRP on the N1 or N2 to the intersection point
+    """
+    # Is used outside the function
+    global closest_point
+    # Creates an empty dataframe that will be filled with points close to the intersection point
+    empty_dataframe = gpd.GeoDataFrame(columns=list(gdf.columns) + ["intersec_to"], crs="EPSG:4326")
+    # Loops over the intersection point indexes
     for intersect_index_N1 in intersect_data.index:
         # Gets the point corresponding to the index
-        intersect_point_N1 = intersect_data[intersect_index_N1]
+        intersect_point_n1 = intersect_data[intersect_index_N1]
         # Defines the distance between the intersection point and the closest point
-        min_dist = 10000
-        # Loops over the the N road point indexes
+        smallest_distance = 10000
+        # Loops over the N road point indexes
         for N_index in gdf_data.index:
             # Gets the point corresponding to the index
-            N_point = gdf_data.loc[N_index, "geometry"]
+            n1_point = gdf_data.loc[N_index, "geometry"]
             # Calculates the distance between the two points
-            dist = distance(N_point, intersect_point_N1)
-            # Checks if distance is smaller then the smallest value already found, if True updates the smallest
+            dist = distance(n1_point, intersect_point_n1)
+            # Checks if distance is smaller than the smallest value already found, if True updates the smallest
             # distance and current closest point
-            if dist < min_dist:
-                min_dist = dist
+            if dist < smallest_distance:
+                smallest_distance = dist
                 closest_point = N_index
-        # Add the index of the examined road
-        road_name_list.append(intersect_index_N1)
-        # Add the id of the closest point
-        closest_points.append(closest_point)
+        # Gets LRP data corresponding to index
+        series = gdf_data.loc[closest_point]
+        # Add a colum to the LRP data to where the LRP intersects to
+        series["intersec_to"] = intersect_index_N1
+        # Add to the dataframe
+        empty_dataframe.loc[closest_point] = series
+    # Change the type for all the points to intersection
+    empty_dataframe["type"] = "intersection"
 
-# Uses the indexes to get the rows corresponding to the closest rows and puts it in a list
-df_list.append(gdf[gdf.index.isin(closest_points)])
-# Creates one dataframe from the df_list
-df_intersections_main = pd.concat(df_list, axis=0, ignore_index=True)
-# Adds a column with the road names to which the intersection belongs
-df_intersections_main["intersec_to"] = road_name_list
-# Updates the type of the points
-df_intersections_main["type"] = "intersection"
-# Saves the df
+    return empty_dataframe
 
-output_csv_path = os.path.join(project_root, 'data', 'intersections_main.csv')
-df_intersections_main.to_csv(output_csv_path, index=False)
 
-###
-###
-###
+# Find the LRPs the N1 and N2
+df_intersections_main_N1 = process_intersection_data(gdf_N1, intersections_N1)
+df_intersections_main_N2 = process_intersection_data(gdf_N2, intersections_N2)
+# Merge the result into one dataframe
+df_intersections_main = pd.concat([df_intersections_main_N1, df_intersections_main_N2], axis=0, ignore_index=True)
+# Store dataframe
+df_intersections_main.to_csv(main_folder_path + "\\data\\intersections_main.csv")
 
-###
-### Gets the closest LRP on the roads that intersect with the N1 and N2
-###
-
-# Will contain the rows of the points on the N1 closest the intersection
-df_list = []
-# Loops over the road names that intersect with the N1
+# Creates empty dataframe that will contain the LRPs on the side roads that intersect to the N1
+Side_to_N1 = gpd.GeoDataFrame(columns=list(gdf.columns) + ["intersec_to"], crs="EPSG:4326")
+# Loops over the intersection point indexes
 for road_name in intersections_N1.index:
-    # Selects the rows corresponding to the road name
+    # Subsets the data corresponding to the index of the road
     road_gdf = gdf[gdf["road"] == road_name]
     # Gets the point of the intersection
     intersect_point_N1 = intersections_N1[road_name]
     # Defines the distance between the intersection point and the closest point
     min_dist = 10000
-    # Loops over the point indexes that correspond to the road that intersects with the N1
+    # Loops over the N road point indexes
     for road_point_index in road_gdf.index:
         # Gets the point corresponding to the index
         road_point = road_gdf.loc[road_point_index, "geometry"]
@@ -163,10 +145,16 @@ for road_name in intersections_N1.index:
         if dist < min_dist:
             min_dist = dist
             closest_point = road_point_index
-    # Gets the row corresponding to the closest point
-    df_list.append(road_gdf[road_gdf.index == closest_point])
+    # Gets LRP data corresponding to index
+    series = road_gdf.loc[closest_point]
+    # Add a colum to the LRP data to where the LRP intersects to, which, in
+    # this case is always the N1
+    series["intersec_to"] = "N1"
+    # Add the LRP data to the empty dataframe
+    Side_to_N1.loc[closest_point] = series
 
-# Works the same as code above but then for the N2
+# Same as code above, but then for the N2
+Side_to_N2 = gpd.GeoDataFrame(columns=list(gdf.columns) + ["intersec_to"], crs="EPSG:4326")
 for road_name in intersections_N2.index:
     road_gdf = gdf[gdf["road"] == road_name]
     intersect_point_N2 = intersections_N2[road_name]
@@ -177,14 +165,51 @@ for road_name in intersections_N2.index:
         if dist < min_dist:
             min_dist = dist
             closest_point = road_point_index
-    #Adds the the row to the same list use for the N1
-    df_list.append(road_gdf[road_gdf.index == closest_point])
+    series = road_gdf.loc[closest_point]
+    series["intersec_to"] = "N2"
+    Side_to_N2.loc[closest_point] = series
 
-
-#Merges the rows into a dataframe
-df_intersections_side = pd.concat(df_list, axis=0, ignore_index=True)
-#Updates the type column
+# Joins the dataframes for the N1 and N2 into one
+df_intersections_side = pd.concat([Side_to_N1, Side_to_N2], axis=0, ignore_index=True)
+# Changes the type to intersection
 df_intersections_side["type"] = "intersection"
 
-output_csv_path = os.path.join(project_root, 'data', 'intersections_side.csv')
-df_intersections_side.to_csv(output_csv_path, index=False)
+# The N208 and N207 are the only side roads with an intersection
+
+# Creates subsets for the N207 and N208
+gdf_N208 = gdf[gdf["road"] == "N208"]
+gdf_N207 = gdf[gdf["road"] == "N207"]
+
+# Select the LRP closest point, value is manually picked
+gdf_N208_closest_point = gdf_N208.loc[3512]
+gdf_N208_closest_point["intersec_to"] = "N207"
+
+# The value on the N207 can not be manually picked
+
+# Will be used to determine which distance is the smallest
+min_dist = 10000
+for N_index in gdf_N207.index:
+    # Selects the corresponding point to the index
+    N1_point = gdf_N207.loc[N_index, "geometry"]
+    # Calculates the distance
+    dist = distance(N1_point, gdf_N208_closest_point.loc["geometry"])
+    # Check if distance is smaller then the already smallest distance
+    if dist < min_dist:
+        # If yes, updates the smallest distance
+        min_dist = dist
+        # Assign new index of closest point
+        closest_point = N_index
+# Get LRP data corresponding to index
+gdf_N207_closest_point = gdf_N207.loc[closest_point]
+# Adds value to indicate to which road the intersection is
+gdf_N207_closest_point["intersec_to"] = "N208"
+
+# Creates a dataframe of all the intersection data
+df_intersections_all = pd.concat([df_intersections_main,
+                                  df_intersections_side,
+                                  gpd.GeoDataFrame(gdf_N207_closest_point).T,
+                                  gpd.GeoDataFrame(gdf_N208_closest_point).T], axis=0, ignore_index=True)
+
+# Saves the intersection data to a csv
+main_folder_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+df_intersections_all.to_csv(main_folder_path + "\\data\\intersections.csv")
